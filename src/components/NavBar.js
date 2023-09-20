088f6efbc224f4259f0a73c0c78d3f8e481d2e72
Ahmed Khemici <ahmed.khemici@yib.io>
2023-09-20 16:00:43 +0100

    update nave bar and slider

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,43 +3,47 @@ import classes from './NavBar.module.css';
 
 const MainNavigation = () => {
   return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-              end
-            >
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/Accueil"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Accueil
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/Offers"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              Offers
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className={classes.header}>
+      <div className={classes['contact-info']}>
+      </div>
+      <header className={classes['nav-bar']}>
+        <nav>
+          <ul className={classes.list}>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+                end
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Accueil"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Accueil
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Offers"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Offers
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
   );
 }
 
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,6 +3,7 @@ import classes from './Slider.module.css'
 const Slider = () => {
     return (
       <>
+        <div className={classes['nav-bar']}></div>
         <div className={classes.slider}>
             Slider
         </div>   
