344a2bf7b3d85acb0e1833f59894702cb6c2c7bc
Ahmed Khemici <ahmed.khemici@yib.io>
2023-09-23 15:36:43 +0100

    added code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,60 +5,62 @@ import Button from './UI/Button';
 const NavBar = () => {
   return (
       <nav className={classes['nav-bar']}>
-        <div className=''>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? classes.active : undefined
-            }
-            end
-          >
-          <img src={`${process.env.PUBLIC_URL}/img/logo.png`} className={classes.logo} alt='Logo'/>
-          </NavLink>
+        <div className={classes.container}>
+          <div className="logo">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              <img src={`${process.env.PUBLIC_URL}/img/logo.png`} className={classes.logo} alt='Logo'/>
+            </NavLink>
+          </div>
+          <ul>
+            <li>
+              <NavLink
+                to="/Pricing"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                PRICING
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/FAQ"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                FAQ
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/contact-us"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                CONTACT US
+              </NavLink>  
+            </li>          
+            <li>
+              <NavLink
+                to="/contact-us"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                CONTACT US
+              </NavLink>
+            </li>
+              <Button className={classes.button}>Free Trail</Button>
+          </ul>
         </div>
-        <ul>
-          <li>
-            <NavLink
-              to="/Pricing"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              PRICING
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/FAQ"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              FAQ
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              CONTACT US
-            </NavLink>  
-          </li>          
-          <li>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) =>
-                isActive ? classes.active : undefined
-              }
-            >
-              CONTACT US
-            </NavLink>
-          </li>
-            <Button className={classes.button}>Free Trail</Button>
-        </ul>
       </nav>
   );
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,11 +1,11 @@
 import classes from './Slider.module.css'
 
 const Slider = () => {
-  // const image1 = `${process.env.PUBLIC_URL}/images/slider_1.jpg`
+  const image1 = `${process.env.PUBLIC_URL}/images/slider_1.jpg`
   return (
     <>
       <div className={classes.slider}>
-          {/* <img src={image1} alt='Slide'/> */}
+        <img src={image1} alt='Slide'/>
       </div>   
     </>
   );
